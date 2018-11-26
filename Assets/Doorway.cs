f6c73aab6a6cbbda0739383996c7235f8c163679
Noah <41136053+Nordtwig@users.noreply.github.com>
2018-11-26 15:52:18 +0100

    Added Doorway functionality.

---
+++ Assets/Doorway.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doorway : MonoBehaviour {
+
+    public GameObject targetRoom;
+    public GameObject doorModel;
+
+	// Use this for initialization
+	void Start () {
+        doorModel.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            targetRoom.SetActive(true);
+            //doorModel.SetActive(true);
+        }
+    }
+}
