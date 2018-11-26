e0a5f7b90569b95c1101f523c7ce12a5aadfdd1f
VidarVigg <43001588+VidarVigg@users.noreply.github.com>
2018-11-26 14:57:39 +0100

    Added scene, made som controls

---
+++ Assets/PlayerMovementScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour {
+    public int speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.W)) {
+            Debug.Log("test");
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-Vector3.forward * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left*Time.deltaTime*speed);
+        }
+	}
+}
