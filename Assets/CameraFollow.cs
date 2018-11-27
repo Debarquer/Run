88c00bdc1529ec2347288b5e972d5a4f0e90eea2
Simon Ektjärn <debarquer@gmail.com>
2018-11-27 13:37:21 +0100

    Added my player prefab

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -12,7 +12,7 @@ public class CameraFollow : MonoBehaviour {
      playerObj = GameObject.Find("Player");
     }
 
-    void FixedUpdate()
+    void LateUpdate()
     {
         transform.position = playerObj.transform.position + offset;
     }
