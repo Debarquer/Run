401660cb6c53524aae2cedbf567d3e2a08c9238e
Heimer Ahnfelt <Thekingofspades@live.se>
2018-11-27 13:17:50 +0100

    Added alternative movement systems

--- Assets/PlayerMovementScript.cs
+++ Assets/PlayerMovementScript.cs
@@ -12,24 +12,47 @@ public class PlayerMovementScript : MonoBehaviour {
     }
 	
 
-	void Update () {
-        // https://answers.unity.com/questions/1190224/how-can-i-normalize-2d-vectors.html
+	void FixedUpdate () {
+        AddForceMovement();
+    }
 
+    void VelocityMovement()
+    {
         Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
         direction.Normalize();
-        transform.Translate(direction* Time.deltaTime *speed);
-        if (direction == Vector3.zero) {
-            //SnapPosition();
-        }
+        GetComponent<Rigidbody>().velocity = new Vector3(direction.x * speed, GetComponent<Rigidbody>().velocity.y, direction.z * speed);
+    }
 
+    void AddForceMovement()
+    {
+        Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        direction.Normalize();
+        GetComponent<Rigidbody>().AddForce(direction, ForceMode.Impulse);
     }
 
     private void SnapPosition() {
         Vector3 snapTarget = new Vector3(Mathf.Round(transform.position.x), transform.position.y, Mathf.Round(transform.position.z));
         transform.position = Vector3.Lerp(transform.position, snapTarget, snapSpeed);
     }
+
+    void TranslateMovement()
+    {
+        // https://answers.unity.com/questions/1190224/how-can-i-normalize-2d-vectors.html
+
+        Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        direction.Normalize();
+        transform.Translate(direction * Time.deltaTime * speed);
+        if (direction == Vector3.zero)
+        {
+            //SnapPosition();
+        }
+
+    }
 }
+
 /*  if (Input.GetKey(KeyCode.W)) {
      Debug.Log("test");
      transform.Translate((Vector3.forward * Time.deltaTime ).normalized *speed);
