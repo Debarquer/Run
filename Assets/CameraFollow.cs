1029ae0753698304a8b3eddc38ca250c467b2158
VidarVigg <43001588+VidarVigg@users.noreply.github.com>
2018-11-27 11:51:18 +0100

    Main camera follow

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    GameObject playerObj;
+    public Vector3 offset;
+    
+
+   void Start()
+    {
+     playerObj = GameObject.Find("Player");
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = playerObj.transform.position + offset;
+    }
+
+}
