d803ccad72e3156eb7dd9fc21bf617e9c13ed959
Simon Ektjärn <debarquer@gmail.com>
2018-12-03 14:10:36 +0100

    Moved moving object to child object

---
+++ Assets/ChildObjectOnTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildObjectOnTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.parent = this.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (other.transform.parent == this.transform)
+            {
+                other.transform.parent = null;
+            }
+        }
+    }
+}
--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -65,23 +65,4 @@ public class MovingObject : MonoBehaviour {
             }
         }        
     }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            other.transform.parent = this.transform;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            if(other.transform.parent == this.transform)
-            {
-                other.transform.parent = null;
-            }
-        }
-    }
 }
