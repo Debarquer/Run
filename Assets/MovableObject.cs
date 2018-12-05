5118856af4af1f46e87ac5981719a4766ec714f1
Simon Ektjärn <debarquer@gmail.com>
2018-12-05 13:40:08 +0100

    Added events to pressure plates

---
+++ Assets/MovableObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableObject : MonoBehaviour {
+
+    public PressurePlateScript pss;
+
+	// Use this for initialization
+	void Start () {
+        pss.OnEnter += OnTriggered;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggered()
+    {
+        transform.position = new Vector3(transform.position.x + 1, transform.position.y + 1, transform.position.z + 1);
+    }
+}
--- Assets/PressurePlateScript.cs
+++ Assets/PressurePlateScript.cs
@@ -3,18 +3,75 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PressurePlateScript : MonoBehaviour {
-    
+
+    bool hasBeenTriggered = false;
+    public bool MultiTrigger = false;
+    public float triggerTimerMax = 1.0f;
+    float triggerTimer = 0;
+
     public Material triggered;
     public Material Default;
     GameObject plate;
+
+    public delegate void PressurePlateEnterDelegate();
+    public event PressurePlateEnterDelegate OnEnter;
+
+    public delegate void PressurePlateExitDelegate();
+    public event PressurePlateExitDelegate OnExit;
+
+    private void Start()
+    {
+        int number = 0;
+        number += 5;
+
+        OnEnter += Test;
+        OnEnter += Test2;
+    }
+
+    private void Update()
+    {
+        if (hasBeenTriggered && MultiTrigger)
+        {
+            triggerTimer += Time.deltaTime;
+            if(triggerTimer > triggerTimerMax)
+            {
+                triggerTimer = 0;
+                hasBeenTriggered = false;
+            }
+        }
+    }
+
+    public void Test()
+    {
+        Debug.Log("testing");
+    }
+
+    public void Test2()
+    {
+        Debug.Log("testing2");
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if(hasBeenTriggered)
         {
+            return;
+        }
+        else
+        {
+            hasBeenTriggered = true;
 
-            plate = GameObject.FindGameObjectWithTag("Plate");
-            plate.GetComponent<Renderer>().material = triggered;
+            Debug.Log("Help I am triggered");
 
+            if (other.tag == "Player")
+            {
+
+                plate = GameObject.FindGameObjectWithTag("Plate");
+                plate.GetComponent<Renderer>().material = triggered;
+
+            }
+
+            OnEnter();
         }
     }
     private void OnTriggerExit(Collider other)
