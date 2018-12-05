5eef2c6c9038a5db76eedafd77729e3e64cb7679
Heimer Ahnfelt <thekingofspades@live.se>
2018-12-05 11:37:57 +0100

    Added rudimetary Teleport and Activate platform trigger

---
+++ Assets/Scripts/ActivateObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateObject : MonoBehaviour {
+
+    public GameObject target;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            target.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/TeleportPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPlayer : MonoBehaviour {
+
+    public Transform exit;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player")
+        {
+            other.transform.position = exit.transform.position;
+        }
+    }
+}
