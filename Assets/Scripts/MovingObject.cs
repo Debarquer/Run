16b489e58d8d5a145de341dec45f8af9181c3590
VidarVigg <43001588+VidarVigg@users.noreply.github.com>
2018-12-06 11:43:27 +0100

    Added functionality for moving platform

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -17,9 +18,33 @@ public class MovingObject : MonoBehaviour {
     public float stopCurr;
 
     public float speed = 0f;
+    public bool PressurePlateActivateOption;
+    public bool RepeatOption;
+    public bool isActive = false;
+    public PressurePlateScript pressureplateScript;
 
-	// Use this for initialization
-	void Start () {
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        if (PressurePlateActivateOption)
+        {
+            pressureplateScript.OnEnter += Activate;
+            isActive = false;
+        }
+        StartMoving();
+        
+	}
+
+    private void Activate()
+    {
+        isActive = true;
+    }
+
+    void StartMoving()
+    {
         speed = Vector3.Distance(PositionA.position, PositionB.position) / moveTimer;
         if (!reverseDirection)
         {
@@ -29,36 +54,66 @@ public class MovingObject : MonoBehaviour {
         {
             transform.position = PositionA.transform.position;
         }
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        moveCurr += Time.deltaTime;
-        if(moveCurr > moveTimer)
-        {
-            stopCurr += Time.deltaTime;
-            //if (reverseDirection)
-            //{
-            //    transform.position = PositionB.position;
-            //}
-            //else
-            //{
-            //    transform.position = PositionA.position;
-            //}
 
-            if (stopCurr > stopTimer)
+    }
+
+    // Update is called once per frame
+
+    void FixedUpdate () {
+        if (!isActive)
+        {
+            return;
+        }
+
+        if (RepeatOption == true) { 
+            moveCurr += Time.deltaTime;
+            if (moveCurr > moveTimer)
             {
-                stopCurr = 0;
-                moveCurr = 0;
+                stopCurr += Time.deltaTime;
+
                 if (reverseDirection)
                 {
-                    moveTimer = moveTimerMaxA;
-                    reverseDirection = false;
+                    transform.position = PositionB.position;
                 }
                 else
                 {
-                    moveTimer = moveTimerMaxB;
-                    reverseDirection = true;
+                    transform.position = PositionA.position;
+                }
+
+                    if (stopCurr > stopTimer)
+                {
+                    stopCurr = 0;
+                    moveCurr = 0;
+                    if (reverseDirection)
+                    {
+                        moveTimer = moveTimerMaxA;
+                        reverseDirection = false;
+                    }
+                    else
+                    {
+                        moveTimer = moveTimerMaxB;
+                        reverseDirection = true;
+                    }
+                }
+            }
+            else
+            {
+                //calculate what speed is needed to traverse from a to b in the correct time
+                if (!reverseDirection)
+                {
+                    float moveSpeed = Vector3.Distance(PositionA.position, PositionB.position) / moveTimerMaxA;
+
+                    transform.Translate((PositionA.position - transform.position).normalized * Time.deltaTime * moveSpeed);
+
+                    //transform.localPosition = Vector3.Lerp(PositionB.localPosition, PositionA.localPosition, (moveCurr % moveTimer) / moveTimer);
+                }
+                else
+                {
+                    float moveSpeed = Vector3.Distance(PositionA.position, PositionB.position) / moveTimerMaxB;
+
+                    transform.Translate((PositionB.position - transform.position).normalized * Time.deltaTime * moveSpeed);
+
+                    //transform.localPosition = Vector3.Lerp(PositionA.localPosition, PositionB.localPosition, (moveCurr % moveTimer) / moveTimer);
                 }
             }
         }
