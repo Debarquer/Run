cfc14803f933d23aaf63dba55afaf53cf8ac9769
VidarVigg <vidar.martensson@outlook.com>
2018-12-13 12:11:15 +0100

    Cleaned up Green Level

--- Assets/FallingCubeTriggerScript.cs
+++ Assets/FallingCubeTriggerScript.cs
@@ -6,10 +6,18 @@ public class FallingCubeTriggerScript : MonoBehaviour {
     public GameObject[] thoseAboutToFall;
     public bool ApplyForce;
     public bool TriggerOnce;
+    float force = 650f;
     [SerializeField] bool sprayRight;
     [SerializeField] bool sprayLeft;
+    [SerializeField] bool morePower;
+    private void Start()
+    {
+        if (morePower)
+        {
+            force = force * 2;
+        }
+    }
 
-   
     private void OnTriggerEnter(Collider other)
     {
         if (TriggerOnce)
@@ -20,10 +28,12 @@ public class FallingCubeTriggerScript : MonoBehaviour {
                 foreach (var rb in thoseAboutToFall)
                 {
                     rb.AddComponent<Rigidbody>();
+
                     if (ApplyForce)
                     {
+                        
                         Rigidbody rigidbody = rb.GetComponent<Rigidbody>();
-                        rigidbody.AddForce(transform.up * 650);
+                        rigidbody.AddForce(transform.up * force);
                         if (sprayRight)
                         {
                         rigidbody.AddForce(transform.right * 100);
