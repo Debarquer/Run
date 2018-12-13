1c50cf797114d0281fff5bcff3db818c1c274fc6
Heimer Ahnfelt <thekingofspades@live.se>
2018-12-13 14:29:47 +0100

    Added Doorway Spotlights with dimmer and updated

---
+++ Assets/LightDimmer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightDimmer : MonoBehaviour {
+    Light lt;
+    bool dimmingActive;
+    float playerDist = 20f;
+    float duration = 3.0f;
+    public float intensityOffset = 7;
+
+    public Transform player;
+
+    void Start () {
+        lt = GetComponent<Light>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (dimmingActive)
+        {
+            playerDist = Vector3.Distance(player.position, transform.position);
+            //Debug.Log("distance = " + playerDist);
+            //var amplitude = Mathf.PingPong(Time.time, duration);           
+        }
+        lt.intensity = (2f * playerDist) - 7f;
+        // 
+        //Debug.Log("Distance is: " + playerDist);
+    }
+
+    private void OnTriggerEnter (Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            dimmingActive = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            dimmingActive = false;
+        }
+    }
+}
--- Assets/Scripts/Doorway.cs
+++ Assets/Scripts/Doorway.cs
@@ -6,24 +6,27 @@ public class Doorway : MonoBehaviour {
 
     public GameObject targetRoom;
     public GameObject doorModel;
-    public GameObject lightEffect;
+    public GameObject lightSource;
 
 	// Use this for initialization
 	void Start () {
+        targetRoom.SetActive(false);
         //doorModel.SetActive(false);
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 
     private void OnTriggerEnter(Collider other) {
         if (other.tag == "Player") {
-            targetRoom.SetActive(true);
-            if (lightEffect != null) {
-                lightEffect.SetActive(false);
+            if (lightSource != null)
+            {
+                lightSource.SetActive(false);
             }
+            targetRoom.SetActive(true);
+            
             //doorModel.SetActive(true);
 
         }
