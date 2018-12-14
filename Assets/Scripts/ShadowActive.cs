6fe1c749cc594fc8c05f0b928e5eda267df345d4
VidarVigg <vidar.martensson@outlook.com>
2018-12-14 14:49:34 +0100

    Added Shadow Movement to green level
    Doorways need to be applied to optimize level tho

--- Assets/Scripts/ShadowActive.cs
+++ Assets/Scripts/ShadowActive.cs
@@ -1,14 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ShadowActive : MonoBehaviour {
 
     public GameObject targetEnemy;
+    public Transform spawnpoint;
+    public bool shadowActivate;
+
+
 
     private void OnTriggerEnter(Collider other) {
-        if (other.tag == "Player") {
-            targetEnemy.SetActive(true);
+      
+        
+            
+        if (other.tag == "Player")
+        {
+            
+
+            if (shadowActivate == true)   
+            {
+                if (targetEnemy.activeInHierarchy)
+                {
+                    Teleport();
+                }
+                else
+                {
+                    targetEnemy.SetActive(true);
+
+                    Teleport();
+
+                }
+            }
+            else if (shadowActivate == false)
+            {
+                targetEnemy.SetActive(false);
+            }
         }
+
+    }
+
+    private void Teleport()
+    {
+        targetEnemy.GetComponent<NavMeshAgent>().Warp(spawnpoint.transform.position);
+
     }
 }
