d74e5533282765ed9c1a93e765be2617c6d5ccf1
Simon Ektjärn <debarquer@gmail.com>
2018-12-14 14:55:29 +0100

    More work on red level, added catapult, added pos round script

---
+++ Assets/Catapult.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catapult : MonoBehaviour {
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit " + other.transform.name);
+        if (other.transform.tag == "Player")
+        {
+            Debug.Log("Hit Player");
+            StartCoroutine(CannonizePlayer(other.transform));
+        }
+    }
+
+    IEnumerator CannonizePlayer(Transform t)
+    {
+        Debug.Log("CannonizingPlayer");
+
+        Vector3 startPos = t.position;
+        Vector3 endPos = new Vector3(startPos.x, startPos.y, startPos.z + 55);
+
+        for (float i = 0; i < 1; i += Time.deltaTime/2)
+        {
+            Debug.Log("Looping");
+
+            t.position = Vector3.Lerp(startPos, endPos, i);
+
+            yield return null;
+        }
+    }
+}
--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -179,19 +179,19 @@ public class MovingObject : MonoBehaviour {
         }        
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "Player")
-        {
-            other.transform.parent = this.transform;
-        }
-    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if(other.tag == "Player")
+    //    {
+    //        other.transform.parent = this.transform;
+    //    }
+    //}
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            other.transform.parent = null;
-        }
-    }
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.tag == "Player")
+    //    {
+    //        other.transform.parent = null;
+    //    }
+    //}
 }
