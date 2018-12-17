a1b4f1269bcfac7e2dba64681e3f481e11ae2fff
Simon Ektjärn <debarquer@gmail.com>
2018-12-18 00:49:37 +0100

    Added levelselect sceneloading to red level

---
+++ Assets/DoorwayCloser.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorwayCloser : MonoBehaviour {
+
+    public GameObject targetRoom;
+    //public GameObject doorModel;
+
+    public delegate void DoorClosedDelegate();
+    public event DoorClosedDelegate OnDoorClosed;
+
+    // Use this for initialization
+    void Start()
+    {
+        //targetRoom.SetActive(false);
+        //doorModel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            targetRoom.SetActive(false);
+
+            if(OnDoorClosed != null)
+                OnDoorClosed();
+
+            Destroy(this);
+
+            //doorModel.SetActive(true);
+
+        }
+    }
+}
---
+++ Assets/Scripts/LoadSceneAsync.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneAsync : MonoBehaviour {
+
+    public DoorwayCloser doorwayCloser;
+    public string sceneName;
+
+	// Use this for initialization
+	void Start () {
+        doorwayCloser.OnDoorClosed += LoadNextLevel;
+	}
+	
+	public void LoadNextLevel()
+    {
+        Vector3 roomStartPos = transform.parent.position;
+        Camera.main.transform.position = Camera.main.transform.position - roomStartPos;
+        FindObjectOfType<Player>().transform.position = FindObjectOfType<Player>().transform.position - roomStartPos;
+        this.transform.parent.position -= roomStartPos;
+
+        StartCoroutine(StartLoading());
+    }
+
+    IEnumerator StartLoading()
+    {
+        AsyncOperation asyncOperation = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("RedLevel", LoadSceneMode.Additive);
+        asyncOperation.allowSceneActivation = false;
+        while (!asyncOperation.isDone)
+        {
+            //Output the current progress
+            Debug.Log("Loading progress: " + (asyncOperation.progress * 100) + "%");
+
+            if (asyncOperation.progress >= 0.9f)
+            {
+                SceneManager.MoveGameObjectToScene(FindObjectOfType<Player>().gameObject, SceneManager.GetSceneByName(sceneName));
+                SceneManager.MoveGameObjectToScene(Camera.main.gameObject, SceneManager.GetSceneByName(sceneName));
+                asyncOperation.allowSceneActivation = true;
+                transform.parent.gameObject.SetActive(false);
+            }
+
+            yield return null;
+        }
+    }
+}
