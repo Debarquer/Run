2b6716f45c7564b015faafb57bbebcede2323275
Simon Ektjärn <debarquer@gmail.com>
2018-12-18 15:29:19 +0100

    Before testing change on green level

---
+++ Assets/ChangeWallMaterial.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeWallMaterial : MonoBehaviour {
+
+    public MeshRenderer a;
+    public MeshRenderer b;
+    public MeshRenderer c;
+    public Material window;
+    public DoorwayCloser doorwayCloser;
+
+    // Use this for initialization
+    void Start () {
+        doorwayCloser.OnDoorClosed += OnDoorClosed;
+	}
+
+    public void OnDoorClosed()
+    {
+        a.material = window;
+        b.material = window;
+        c.material = window;
+    }
+}
