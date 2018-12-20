00a96417f70445ceef9b9bf7d7784d1f3a44d9fa
Simon Ektjärn <debarquer@gmail.com>
2018-12-20 10:55:58 +0100

    Added more lights to the level select scene
    The lights now enable/disable depending on if the level has been completed or not

---
+++ Assets/LevelSelectEntranceLamps.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectEntranceLamps : MonoBehaviour {
+
+    public Material emissiveMaterial;
+    public Material nonEmissiveMaterial;
+    //public GameObject lamps;
+    public string levelName;
+
+	// Use this for initialization
+	void Start () {
+		if(PlayerPrefs.GetInt(levelName) == 0)
+        {
+            GetComponent<Renderer>().material = nonEmissiveMaterial;
+            GetComponent<Light>().enabled = true;
+            //lamps.SetActive(false);
+        }
+        else if(PlayerPrefs.GetInt(levelName) == 1)
+        {
+            GetComponent<Renderer>().material = emissiveMaterial;
+            //lamps.SetActive(true);
+            GetComponent<Light>().enabled = false;
+        }
+        else
+        {
+            Debug.Log("LevelSelectEntranceLamps error: Invalid playerprefs result");
+        }
+	}
+}
