f194c5ce5addea59b000950b6d2c242222a0411f
Simon Ektjärn <debarquer@gmail.com>
2018-12-20 12:25:20 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/SubmitHighscoreScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubmitHighscoreScript : MonoBehaviour {
+
+    Text scoreText;
+    InputField nameField;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.visible = true;
+    }
+
+    public void Highscore()
+    {
+        Cursor.visible = false;
+
+        string currentLevel = FindObjectOfType<GameController>().CurrentLevel;
+        FindObjectOfType<HighscoreController>
+    }
+}
