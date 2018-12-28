4ee15310ea9f40fcfdab92a7f8c2125a684feb8b
Noah <nordqvist.gaming@gmail.com>
2018-12-28 11:50:13 +0100

    Started Options implementation.

--- Assets/Scripts/OptionsController.cs
+++ Assets/Scripts/OptionsController.cs
@@ -1,19 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class OptionsController : MonoBehaviour {
     public GameObject MainMenu;
     public GameObject OptionsMenu;
+    public Image fullScreenCheckmark;
+    public Image muteAudioCheckmark;
+    public Image speedModeCheckmark;
+
     MoveCamera main;
     private void Start()
     {
         main = Camera.main.gameObject.GetComponent<MoveCamera>();
+        //ToggleCheckmarkColor(fullScreenCheckmark, PlayerPrefs.GetInt("fullscreen")); 
+        ToggleCheckmarkColor(fullScreenCheckmark, 1);
     }
-    public void Button1()
+    public void ToggleFullscreen()
     {
-
+        ToggleCheckmarkColor(fullScreenCheckmark, 0);
     }
     public void Button2()
     {
@@ -23,11 +30,18 @@ public class OptionsController : MonoBehaviour {
     {
 
     }
-    public void Button4()
+    public void Back()
     {
         MainMenu.SetActive(true);
         OptionsMenu.SetActive(false);
         main.MoveToMainMenu();
         
+    }
+
+    public void ToggleCheckmarkColor(Image checkmark, int value) {
+        if (value == 1)
+            checkmark.color = Color.green;
+        if (value == 0)
+            checkmark.color = Color.red;
     }
 }
