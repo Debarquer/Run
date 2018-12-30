b6f9ed6a8f84f9885e8a17f75afcc50f96c40c13
VidarVigg <vidar.martensson@outlook.com>
2018-12-30 18:03:12 +0100

    added sound to red level

---
+++ Assets/FallingBridgeSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBridgeSound : MonoBehaviour {
+
+    [FMODUnity.EventRef]
+    public string boomSound;
+
+    bool hasFallen = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (hasFallen == false)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(boomSound);
+            hasFallen = true;
+        }
+    }
+}
