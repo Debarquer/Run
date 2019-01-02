87b6504f9a86a1f90d0421a7a4993070cc05fac3
VidarVigg <vidar.martensson@outlook.com>
2019-01-02 12:18:47 +0100

    Added pushing pillar placeholder sound

---
+++ Assets/CoordinateASoundScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoordinateASoundScript : MonoBehaviour {
+    [FMODUnity.EventRef]
+    public string enter;
+
+    [FMODUnity.EventRef]
+    public string exit;
+  
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+       if (other.tag == "Pillar")
+       {
+            FMODUnity.RuntimeManager.PlayOneShotAttached(enter, this.gameObject);
+            
+       }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Pillar")
+        {
+            FMODUnity.RuntimeManager.PlayOneShotAttached(exit, this.gameObject);
+           
+        }
+       
+    }
+}
---
+++ Assets/CoordinateBSoundScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoordinateBSoundScript : MonoBehaviour {
+    [FMODUnity.EventRef]
+    public string enter;
+
+    [FMODUnity.EventRef]
+    public string exit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pillar")
+        {
+            FMODUnity.RuntimeManager.PlayOneShotAttached(enter, this.gameObject);
+            
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Pillar")
+        {
+            FMODUnity.RuntimeManager.PlayOneShotAttached(exit, this.gameObject);
+            
+        }
+
+    }
+}
