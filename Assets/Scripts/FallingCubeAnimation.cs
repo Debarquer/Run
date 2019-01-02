85c75b9b307133c230f6ef3b8d8ac9ba2efd1db2
Simon Ektjärn <debarquer@gmail.com>
2019-01-02 11:47:28 +0100

    Changed falling bridges to use animations instead of physics

---
+++ Assets/Scripts/FallingCubeAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingCubeAnimation : MonoBehaviour {
+
+    bool hasAnimated = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collided with " + collision.transform.name);
+
+        if(collision.transform.tag == "Player")
+        {
+            if (!hasAnimated)
+            {
+                hasAnimated = true;
+                GetComponentInParent<Animator>().Play("FallingCubeAnimation");
+            }
+
+        }
+    }
+}
