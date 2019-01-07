c27c497ca87627ed97e6b2cda5e90be8a8879652
Heimer Ahnfelt <thekingofspades@live.se>
2019-01-07 14:01:41 +0100

    Made Music Parameter Trigger Script

---
+++ Assets/MusicParameterTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicParameterTrigger : MonoBehaviour {
+
+    FMODUnity.StudioEventEmitter musicEmitter;
+    public bool MakeAmbient;
+    
+	void OnEnable () {
+        GameObject GameobjectTarget = GameObject.Find("GameController");
+        musicEmitter = GameobjectTarget.GetComponent<FMODUnity.StudioEventEmitter>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (MakeAmbient)
+        {
+            musicEmitter.SetParameter("AmbientMode", 1);
+        }
+        else if (!MakeAmbient)
+        {
+            musicEmitter.SetParameter("AmbientMode", 0);
+        }
+    }
+}
