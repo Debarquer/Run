d6f6dac7d8f6ae35824899c68de79f81a99ee34b
VidarVigg <vidar.martensson@outlook.com>
2019-01-07 14:17:02 +0100

    Added sound to menu

---
+++ Assets/MenuClickSound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuClickSound : MonoBehaviour {
+
+    [FMODUnity.EventRef]
+    public string clickSound;
+
+    public void ClickSound()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(clickSound);
+    }
+}
