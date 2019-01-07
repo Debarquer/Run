3f24130f3566680800ecbfa044ee9c67c2d57772
VidarVigg <vidar.martensson@outlook.com>
2019-01-07 11:00:26 +0100

    Added Death sounds to shadow, blue and green

---
+++ Assets/FallingSoundScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingSoundScript : MonoBehaviour {
+
+    [FMODUnity.EventRef]
+    public string fallingSound;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(fallingSound);
+        }
+    }
+}
