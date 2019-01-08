59d0261511dfb550fd30660e7b743f5c2e7f3b9e
Noah <nordqvist.gaming@gmail.com>
2019-01-08 11:22:52 +0100

    Added Debug View in-game
    Helps with debugging in Builds. Toggle with "F".

---
+++ Assets/DebugView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DebugView : MonoBehaviour {
+
+    public Text debugText;
+    public string output = "";
+    public string stack = "";
+
+    void OnEnable() {
+        debugText.text = "";
+        Application.logMessageReceived += HandleLog;
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void OnDisable() {
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    void Update() {
+        if (Input.GetKeyUp("f"))
+            debugText.enabled = !debugText.enabled;
+    }
+
+    void HandleLog(string logString, string stackTrace, LogType type) {
+        output = logString;
+        stack = stackTrace;
+        debugText.text += "[" + type + "] [" + System.DateTime.Now.ToString("hh:mm:ss.ff") + "] " + output + "\n";
+    }
+}
