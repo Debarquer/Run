163828622f9418af4057502965ae4d8b2ea0b235
Simon Ektjärn <debarquer@gmail.com>
2019-01-09 13:46:45 +0100

    Updated main menu button appearance

---
+++ Assets/ButtonAppearanceChanger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonAppearanceChanger : MonoBehaviour {
+
+    public Material standard;
+    public Material hover;
+    public Material click;
+
+    Image image;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    public void SetStandard()
+    {
+        image.material = standard;
+    }
+
+    public void SetHover()
+    {
+        image.material = hover;
+    }
+    public void SetClick()
+    {
+        image.material = click;
+    }
+}
